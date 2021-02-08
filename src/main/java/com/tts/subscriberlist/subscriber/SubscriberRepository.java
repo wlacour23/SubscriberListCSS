9373cfcf3a945558b5efb7018b506a0b484d3ea6
Wade LaCour <74622073+wlacour23@users.noreply.github.com>
2021-02-07 22:15:34 -0600

    Add files via upload
    I meant to do this one with the CSS first.

---
+++ src/main/java/com/tts/subscriberlist/subscriber/SubscriberRepository.java
@@ -0,0 +1,6 @@
+package com.tts.subscriberlist.subscriber;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface SubscriberRepository extends CrudRepository<Subscriber,Long> {
+}
